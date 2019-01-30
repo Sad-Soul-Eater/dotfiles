2bbede3ae34c299efaa5ef91abbe6efc29e2c728
Sad-Soul-Eater <Jarvis2709@gmail.com>
2019-01-31 01:20:36 +0200

    polybar popup-calendar script added

---
+++ configs/bin/polybar-scripts/popup-calendar.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+YAD_WIDTH=200
+YAD_HEIGHT=200
+DATE=" $(date +"%e %B")"
+
+case "$1" in
+    --popup)
+        eval "$(xdotool getmouselocation --shell)"
+	eval "$(xdotool getdisplaygeometry --shell)"
+	
+	# X
+	if (( X + 35 + YAD_WIDTH / 2  > WIDTH )); then	#Right side
+		: $(( pos_x = WIDTH - 35 - YAD_WIDTH ))
+	elif (( X - YAD_WIDTH / 2 < 1 )); then		#Left side
+		: $(( pos_x = 10 ))
+	else						#Center
+		: $(( pos_x = X - YAD_WIDTH / 2 ))
+	fi
+
+	# Y
+	if (( Y + YAD_HEIGHT > HEIGHT )); then		#Bottom
+            : $(( pos_y = HEIGHT - 22 - YAD_HEIGHT ))
+    	else						#Top
+            : $(( pos_y = Y + 22 ))
+    	fi
+
+        yad --calendar --undecorated --fixed --close-on-unfocus --no-buttons \
+            --width=$YAD_WIDTH --height=$YAD_HEIGHT --posx=$pos_x --posy=$pos_y \
+	    --title="Yad Calendar" \
+            > /dev/null
+        ;;
+    *)
+        echo "$DATE"
+        ;;
+esac
