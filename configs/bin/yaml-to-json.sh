5de11c339a0488606f8752312647f0410004e803
Sad-Soul-Eater <Jarvis2709@gmail.com>
2019-02-01 02:58:53 +0200

    new script to move from yaml to json

---
+++ configs/bin/yaml-to-json.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Convert yaml files to json files in specified folder
+# Usage - ./yaml-to-json.sh /dir/to/yaml
+
+WORKING_DIR="$1"
+
+for YAML_FILE in "$WORKING_DIR"/*.yaml; do
+    [ -e "$YAML_FILE" ] || continue
+
+    FILE_NAME=$(basename "${YAML_FILE%.*}")
+    JSON_FILE=$WORKING_DIR/$FILE_NAME.json
+
+    python -c 'import sys, yaml, json; json.dump(yaml.load(sys.stdin), sys.stdout, indent=4)' < "$YAML_FILE" > "$JSON_FILE"
+
+    echo "$FILE_NAME - Converted!"
+done
