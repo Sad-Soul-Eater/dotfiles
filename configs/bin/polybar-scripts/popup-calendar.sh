552a4a2994f56b028846f6af177d00e41f4f27c1
Sad-Soul-Eater <Jarvis2709@gmail.com>
2019-02-23 00:58:19 +0200

    misc improvements & make POSIX compatible

--- configs/bin/polybar-scripts/popup-calendar.sh
+++ configs/bin/polybar-scripts/popup-calendar.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
+BAR_HEIGHT=22
 YAD_WIDTH=200
 YAD_HEIGHT=200
 DATE=" $(date +"%e %B")"
@@ -10,24 +11,24 @@ case "$1" in
     eval "$(xdotool getdisplaygeometry --shell)"
 
     # X
-    if ((X + 35 + YAD_WIDTH / 2 > WIDTH)); then #Right side
+    if [ "$((X + 35 + YAD_WIDTH / 2))" -gt "$WIDTH" ]; then #Right side
         : $((pos_x = WIDTH - 35 - YAD_WIDTH))
-    elif ((X - YAD_WIDTH / 2 < 1)); then #Left side
+    elif [ "$((X - YAD_WIDTH / 2))" -lt 1 ]; then #Left side
         : $((pos_x = 10))
     else #Center
         : $((pos_x = X - YAD_WIDTH / 2))
     fi
 
     # Y
-    if ((Y + YAD_HEIGHT > HEIGHT)); then #Bottom
-        : $((pos_y = HEIGHT - 22 - YAD_HEIGHT))
+    if [ "$((Y + YAD_HEIGHT))" -gt "$HEIGHT" ]; then #Bottom
+        : $((pos_y = HEIGHT - BAR_HEIGHT - YAD_HEIGHT))
     else #Top
-        : $((pos_y = Y + 22))
+        : $((pos_y = Y + BAR_HEIGHT))
     fi
 
     yad --calendar --undecorated --fixed --close-on-unfocus --no-buttons \
         --width=$YAD_WIDTH --height=$YAD_HEIGHT --posx=$pos_x --posy=$pos_y \
-        --title="Yad Calendar" >/dev/null
+        --title="yad-calendar" >/dev/null
     ;;
 *)
     echo "$DATE"
