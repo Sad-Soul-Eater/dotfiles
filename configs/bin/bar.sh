de50c55462c98b4a8fb80b72542dcd3a55dd56b6
Sad-Soul-Eater <Jarvis2709@gmail.com>
2019-06-17 16:53:39 +0300

    add improvements for multi monitor

--- configs/bin/bar.sh
+++ configs/bin/bar.sh
@@ -1,4 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
-killall polybar
-polybar base &
+(
+	flock 200
+
+	killall -q polybar
+
+	while pgrep -u $UID -x polybar >/dev/null; do sleep 0.5; done
+
+	outputs=$(xrandr --query | grep " connected" | cut -d" " -f1)
+	tray_output=eDP
+
+	for m in $outputs; do
+		if [[ $m == "HDMI-A-0" ]]; then
+			tray_output=$m
+		fi
+	done
+
+	for m in $outputs; do
+		export MONITOR=$m
+		export TRAY_POSITION=none
+		if [[ $m == $tray_output ]]; then
+			TRAY_POSITION=right
+		fi
+
+		polybar base </dev/null >/var/tmp/polybar-$m.log 2>&1 200>&- &
+		disown
+	done
+) 200>/var/tmp/polybar-launch.lock
