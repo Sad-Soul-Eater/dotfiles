42d436169c0facac3efe8db092cc789377432646
Sad-Soul-Eater <Jarvis2709@gmail.com>
2019-07-12 07:32:07 +0300

    improve img-optimize scripts

--- configs/bin/img-optimize/optJpg.sh
+++ configs/bin/img-optimize/optJpg.sh
@@ -1,18 +1,73 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-echo "---------------------"
-echo "    Optimizing jpg"
-echo "---------------------"
-echo ""
+optimize() {
+	echo "---------------------"
+	echo "    Optimizing jpg"
+	echo "---------------------"
+	echo ""
 
-if [[ "$1" == "" ]]; then
-	echo "optiJpg.sh [path] [parallel jobs count]"
+	find "$1" -type f -iname '*.jpg' | parallel -j "$2" jpegoptim "$3"
+
+	echo ""
+	echo "---------------------"
+	echo "   Optimizing done"
+	echo "---------------------"
+}
+
+usage() {
+	echo "Usage: ./optiJpg.sh -p \"path_to_imgs\" [options]
+Options:
+-h  open this page
+-p  path to images
+-j  parallel jobs count
+    if not set, used physical CPU count
+-o  options for jpegopti
+    if not set, used -s -o"
+}
+
+if [ "$1" = "" ]; then
+	usage
+	exit 0
+fi
+
+IMGS_PATH=""
+JOBS=""
+OPTS=""
+
+while getopts p:j:o:h OPTION; do
+	case "${OPTION}" in
+	p)
+		IMGS_PATH=${OPTARG}
+		;;
+	j)
+		JOBS=${OPTARG}
+		;;
+	h)
+		usage
+		exit 0
+		;;
+	o)
+		OPTS=${OPTARG}
+		;;
+	*)
+		usage
+		exit 1
+		;;
+	esac
+done
+
+if [ "$IMGS_PATH" = "" ]; then
+	echo "The path is empty"
+	usage
 	exit 1
 fi
 
-find "$1" -type f -iname '*.jpg' | parallel -j "$2" jpegoptim -s -o
+if [ "$JOBS" = "" ]; then
+	JOBS="$(grep '^core id' /proc/cpuinfo | sort -u | wc -l)"
+fi
 
-echo ""
-echo "---------------------"
-echo "   Optimizing done"
-echo "---------------------"
+if [ "$OPTS" = "" ]; then
+	OPTS="-s -o"
+fi
+
+optimize "$IMGS_PATH" "$JOBS" "$OPTS"
--- configs/bin/img-optimize/optPng.sh
+++ configs/bin/img-optimize/optPng.sh
@@ -1,18 +1,72 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-echo "---------------------"
-echo "    Optimizing png"
-echo "---------------------"
-echo ""
+optimize() {
+	echo "---------------------"
+	echo "    Optimizing png"
+	echo "---------------------"
+	echo ""
 
-if [[ "$1" == "" || "$2" -lt 0 ]]; then
-	echo "optiPng.sh [path] [parallel jobs count]"
+	find "$1" -type f -iname '*.png' | parallel -j "$2" optipng "$3"
+
+	echo ""
+	echo "---------------------"
+	echo "   Optimizing done"
+	echo "---------------------"
+}
+
+usage() {
+	echo "Usage: ./optiPng.sh -p \"path_to_imgs\" [options]
+Options:
+-h  open this page
+-p  path to images
+-j  parallel jobs count
+    if not set, used physical CPU count
+-o  options for optipng
+    if not set, used -strip all -fix -clobber"
+}
+
+if [ "$1" = "" ]; then
+	usage
+	exit 0
+fi
+
+IMGS_PATH=""
+JOBS=""
+OPTS=""
+
+while getopts p:j:o:h OPTION; do
+	case "${OPTION}" in
+	p)
+		IMGS_PATH=${OPTARG}
+		;;
+	j)
+		JOBS=${OPTARG}
+		;;
+	h)
+		usage
+		exit 0
+		;;
+	o)
+		OPTS=${OPTARG}
+		;;
+	*)
+		usage
+		exit 1
+		;;
+	esac
+done
+
+if [ "$IMGS_PATH" = "" ]; then
+	usage
 	exit 1
 fi
 
-find "$1" -type f -iname '*.png' | parallel -j "$2" optipng -strip all -fix -clobber
+if [ "$JOBS" = "" ]; then
+	JOBS="$(grep '^core id' /proc/cpuinfo | sort -u | wc -l)"
+fi
 
-echo ""
-echo "---------------------"
-echo "   Optimizing done"
-echo "---------------------"
+if [ "$OPTS" = "" ]; then
+	OPTS="-strip all -fix -clobber"
+fi
+
+optimize "$IMGS_PATH" "$JOBS" "$OPTS"
