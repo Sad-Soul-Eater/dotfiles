8432f1b17c3d2042fd62b97daff59eed1bde9a75
Sad-Soul-Eater <Jarvis2709@gmail.com>
2019-07-30 18:01:14 +0300

    remove magic numbers & fix yad size

--- configs/bin/polybar-scripts/popup-calendar.sh
+++ configs/bin/polybar-scripts/popup-calendar.sh
@@ -3,7 +3,7 @@
 BAR_HEIGHT=22  # polybar height
 BORDER_SIZE=1  # border size from your wm settings
 YAD_WIDTH=222  # 222 is minimum possible value
-YAD_HEIGHT=188 # 188 is minimum possible value
+YAD_HEIGHT=193 # 193 is minimum possible value
 DATE="%{T9}%{T-} $(date +"%e %B")"
 
 case "$1" in
@@ -16,24 +16,24 @@ case "$1" in
 	eval "$(xdotool getdisplaygeometry --shell)"
 
 	# X
-	if [ "$((X + YAD_WIDTH / 2 + 2 + BORDER_SIZE))" -gt "$WIDTH" ]; then #Right side
-		: $((pos_x = WIDTH - YAD_WIDTH - BORDER_SIZE - 4))
-	elif [ "$((X - YAD_WIDTH / 2 - 2 - BORDER_SIZE))" -lt 1 ]; then #Left side
+	if [ "$((X + YAD_WIDTH / 2 + BORDER_SIZE))" -gt "$WIDTH" ]; then #Right side
+		: $((pos_x = WIDTH - YAD_WIDTH - BORDER_SIZE))
+	elif [ "$((X - YAD_WIDTH / 2 - BORDER_SIZE))" -lt 0 ]; then #Left side
 		: $((pos_x = BORDER_SIZE))
 	else #Center
-		: $((pos_x = X - YAD_WIDTH / 2 - 2))
+		: $((pos_x = X - YAD_WIDTH / 2))
 	fi
 
 	# Y
 	if [ "$Y" -gt "$((HEIGHT / 2))" ]; then #Bottom
-		: $((pos_y = HEIGHT - YAD_HEIGHT - 4 - BAR_HEIGHT - BORDER_SIZE))
+		: $((pos_y = HEIGHT - YAD_HEIGHT - BAR_HEIGHT - BORDER_SIZE))
 	else #Top
 		: $((pos_y = BAR_HEIGHT + BORDER_SIZE))
 	fi
 
 	yad --calendar --undecorated --fixed --close-on-unfocus --no-buttons \
 		--width=$YAD_WIDTH --height=$YAD_HEIGHT --posx=$pos_x --posy=$pos_y \
-		--title="yad-calendar" >/dev/null &
+		--title="yad-calendar" --borders=0 >/dev/null &
 	;;
 *)
 	echo "$DATE"
