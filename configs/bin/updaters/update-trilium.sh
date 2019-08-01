e87726ad4d260558905ad8034eda2b291e81ad6e
Sad-Soul-Eater <Jarvis2709@gmail.com>
2019-08-01 07:33:53 +0300

    refactoring

--- configs/bin/updaters/update-trilium.sh
+++ configs/bin/updaters/update-trilium.sh
@@ -7,14 +7,16 @@ fail() {
 
 echo "Update starting!"
 
-TRILIUM_HOME="/home/trilium"
 DOWNLOAD_SCRIPT="$(dirname "$(readlink -f "$0")")/downloaders/download-latest-release-from-github.sh"
 
-cd "$TRILIUM_HOME" || fail
+DIR="/home/trilium"
+FILE="trilium.tar.xz"
+REGEX="browser_download_url.*trilium-linux-x64-server-.*\.tar\.xz"
+OPTS="--dir=$DIR --out=$FILE"
 
 if [ -f "$DOWNLOAD_SCRIPT" ]; then
 	# shellcheck disable=SC1090
-	if ! . "$DOWNLOAD_SCRIPT" "zadam/trilium" "browser_download_url.*trilium-linux-x64-server-.*\.tar\.xz"; then
+	if ! . "$DOWNLOAD_SCRIPT" "zadam/trilium" "$REGEX" "$OPTS"; then
 		fail
 	fi
 else
@@ -22,30 +24,29 @@ else
 	fail
 fi
 
-printf "Stopping Trilium serevice... "
+printf "Stopping Trilium serevice..."
 if ! systemctl stop trilium.service; then
 	fail
 fi
-printf "ok\n"
+printf " ok\n"
 
-printf "Extracting archive... "
-if ! tar -xJf trilium-linux-x64-server-*.tar.xz; then
+printf "Extracting archive..."
+if ! tar -xJf "$DIR/$FILE"; then
 	fail
 fi
-printf "ok\n"
-
-rm trilium-linux-x64-server-*.tar.xz
+rm "$DIR/$FILE"
+printf " ok\n"
 
 printf "Fixing permissions..."
-if ! chown -R trilium:trilium "./trilium-linux-x64-server"; then
+if ! chown -R trilium:trilium "$DIR/trilium-linux-x64-server"; then
 	fail
 fi
-printf "ok\n"
+printf " ok\n"
 
-printf "Starting Trilium serevice... "
+printf "Starting Trilium serevice..."
 if ! systemctl start trilium.service; then
 	fail
 fi
-printf "ok\n"
+printf " ok\n"
 
 echo "Update completed!"
