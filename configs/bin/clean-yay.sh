2b8bc09d0df80c6d4b59cf01a5f84f3ba75f30b5
Sad-Soul-Eater <Jarvis2709@gmail.com>
2020-04-30 02:32:12 +0300

    Bin: add script for yay cache directory cleaning

---
+++ configs/bin/clean-yay.sh
@@ -0,0 +1,26 @@
+#!/bin/env bash
+
+cd "$HOME/.cache/yay" || exit
+CACHE_LIST=$(mktemp)
+INSTALLED_LIST=$(mktemp)
+DIRS_TO_RM=$(mktemp)
+
+cd "$HOME/.cache/yay" || exit
+fd --type=directory --max-depth=1 >"$CACHE_LIST"
+yay -Qq >"$INSTALLED_LIST"
+diff -u "$INSTALLED_LIST" "$CACHE_LIST" | grep -oP "(?<=^\+).*" | grep -vP "(?<=^\+).*" >"$DIRS_TO_RM"
+
+if [ -s "$DIRS_TO_RM" ]; then
+	echo "Founded unneeded folders:"
+	cat "$DIRS_TO_RM"
+	echo ""
+	read -rp "Delete them? <y/N> " prompt
+	if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]; then
+		cd "$HOME/.cache/yay" || exit
+		xargs -a "$DIRS_TO_RM" rm -rfv
+	fi
+else
+	echo "No unneeded folders found!"
+fi
+
+rm "$CACHE_LIST" "$INSTALLED_LIST" "$DIRS_TO_RM"
