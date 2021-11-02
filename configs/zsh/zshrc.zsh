a68b743fca901149f9ceb019fedb7cd06c063376
Mars-2dot <ilya.boldyrev36@gmail.com>
2021-11-02 17:49:50 +0000

    choosing a more correct repository for zinit

--- configs/zsh/zshrc.zsh
+++ configs/zsh/zshrc.zsh
@@ -9,7 +9,7 @@ colors
 if [[ ! -f $HOME/.zinit/bin/zinit.zsh ]]; then
 	print -P "%F{33}▓▒░ %F{220}Installing DHARMA Initiative Plugin Manager (zdharma/zinit)…%f"
 	command mkdir -p "$HOME/.zinit" && command chmod g-rwX "$HOME/.zinit"
-	command git clone https://github.com/treuille/zinit.git "$HOME/.zinit/bin" && \
+	command git clone https://github.com/zdharma-continuum/zinit.git "$HOME/.zinit/bin" && \
 		print -P "%F{33}▓▒░ %F{34}Installation successful.%f" || \
 		print -P "%F{160}▓▒░ The clone has failed.%f"
 fi
