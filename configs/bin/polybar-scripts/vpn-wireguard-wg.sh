ff6fb17d4f800a6d5812836abd1fe70b3f80e6bb
Sad-Soul-Eater <Jarvis2709@gmail.com>
2022-04-06 22:31:48 +0300

    Scripts: use nmcli for VPN connection management

--- configs/bin/polybar-scripts/vpn-wireguard-wg.sh
+++ configs/bin/polybar-scripts/vpn-wireguard-wg.sh
@@ -1,35 +1,22 @@
 #!/usr/bin/env sh
 
+connection_name='hiso-tun'
+
 connection_status() {
-	if [ -f "$config" ]; then
-		connection=$(sudo wg show "$config_name" 2>/dev/null | head -n 1 | awk '{print $NF }')
-
-		if [ "$connection" = "$config_name" ]; then
-			echo "1"
-		else
-			echo "2"
-		fi
-	else
-		echo "3"
-	fi
+	nmcli connection show "$connection_name" | rg -o '(GENERAL\.STATE:\s+)(\w*)' -r '$2'
 }
-
-config="$HOME/wg/hiso-tun.conf"
-config_name=$(basename "${config%.*}")
 
 case "$1" in
 --toggle)
-	if [ "$(connection_status)" = "1" ]; then
-		sudo wg-quick down "$config" 2>/dev/null
+	if [ "$(connection_status)" = 'activated' ]; then
+		nmcli connection down "$connection_name" 2>/dev/null
 	else
-		sudo wg-quick up "$config" 2>/dev/null
+		nmcli connection up "$connection_name" 2>/dev/null
 	fi
 	;;
 *)
-	if [ "$(connection_status)" = "1" ]; then
-		echo "%{T5}󰯄%{T-} $config_name"
-	elif [ "$(connection_status)" = "3" ]; then
-		echo "%{T5}󰒙%{T-} Config not found!"
+	if [ "$(connection_status)" = 'activated' ]; then
+		echo "%{T5}󰯄%{T-} $connection_name"
 	else
 		echo "%{T5}󰒙%{T-}"
 	fi
